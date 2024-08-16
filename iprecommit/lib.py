fe2703d27619a5934ef27671acf249deb53672ad
Ian Fisher <ian@iafisher.com>
2024-08-15 22:17:26 -0400

    respect the NO_COLOR environment variable

--- iprecommit/lib.py
+++ iprecommit/lib.py
@@ -5,6 +5,7 @@ import fnmatch
 import os
 import shlex
 import subprocess
+import sys
 import textwrap
 from dataclasses import dataclass
 from pathlib import Path
@@ -197,20 +198,49 @@ def warn(msg: str) -> None:
     print(f"{yellow('Warning:')} {msg}")
 
 
+# don't access directly; use _has_color() instead
+#
+# once set, this may be reset back to `None` if the module is re-imported elsewhere
+_COLOR = None
+
+
+def _has_color() -> bool:
+    global _COLOR
+
+    if _COLOR is not None:
+        return _COLOR
+
+    _COLOR = not (
+        # https://no-color.org/
+        "NO_COLOR" in os.environ
+        or not os.isatty(sys.stdout.fileno())
+        or not os.isatty(sys.stderr.fileno())
+    )
+
+    return _COLOR
+
+
 def red(s: str) -> str:
-    return f"\033[31m{s}\033[0m"
+    return _colored(s, 31)
 
 
 def yellow(s: str) -> str:
-    return f"\033[33m{s}\033[0m"
+    return _colored(s, 33)
 
 
 def cyan(s: str) -> str:
-    return f"\033[36m{s}\033[0m"
+    return _colored(s, 36)
 
 
 def green(s: str) -> str:
-    return f"\033[32m{s}\033[0m"
+    return _colored(s, 32)
+
+
+def _colored(s: str, code: int) -> str:
+    if not _has_color():
+        return s
+
+    return f"\033[{code}m{s}\033[0m"
 
 
 def _get_changes(*, include_unstaged: bool) -> Changes:
